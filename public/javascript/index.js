4b36accd32ef411101bcda51df730d9af714db16
nodebq <momoxinshang@gmail.com>
2016-02-06 10:33:08 +0800

    header

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -35,3 +35,26 @@ function validate(){
         return false
     }
 }
+
+$(document).ready(function() {
+
+
+
+    /*	2nd example	*/
+
+    $("#menu2 li a").wrapInner( '<span class="out"></span>' );
+
+    $("#menu2 li a").each(function() {
+        $( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+    });
+
+    $("#menu2 li a").hover(function() {
+        $(".out",	this).stop().animate({'top':	'45px'},	200); // move down - hide
+        $(".over",	this).stop().animate({'top':	'0px'},		200); // move down - show
+
+    }, function() {
+        $(".out",	this).stop().animate({'top':	'0px'},		200); // move up - show
+        $(".over",	this).stop().animate({'top':	'-45px'},	200); // move up - hide
+    });
+
+});
