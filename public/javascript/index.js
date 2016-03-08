792194131251c1c41747ef12977fc2be5332a252
nodebq <momoxinshang@gmail.com>
2016-03-08 20:03:44 +0800

    优化alert样式

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -112,14 +112,81 @@ function createCORSRequest(method, url) {
     }
     xhr.open(method, url, false);
     xhr.send();
-    if(xhr.responseText){
+    if (xhr.responseText) {
         return xhr.responseText;
-    }else {
-        return {"code":1005,"message":"不支持xhr,请更换更新版本的浏览器"}
+    } else {
+        return {"code": 1005, "message": "不支持xhr,请更换更新版本的浏览器"}
     }
 }
 
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById("md").click();
 };
+
+(function (c) {
+    var b = {
+        inEffect: {opacity: "show"},
+        inEffectDuration: 600,
+        stayTime: 3000,
+        text: "",
+        sticky: false,
+        type: "notice",
+        position: "top-right",
+        closeText: "",
+        close: null
+    };
+    var a = {
+        init: function (d) {
+            if (d) {
+                c.extend(b, d)
+            }
+        }, showToast: function (f) {
+            var g = {};
+            c.extend(g, b, f);
+            var j, e, d, i, h;
+            j = (!c(".toast-container").length) ? c("<div></div>").addClass("toast-container").addClass("toast-position-" + g.position).appendTo("body") : c(".toast-container");
+            e = c("<div></div>").addClass("toast-item-wrapper");
+            d = c("<div></div>").hide().addClass("toast-item toast-type-" + g.type).appendTo(j).html(c("<p>").append(g.text)).animate(g.inEffect, g.inEffectDuration).wrap(e);
+            i = c("<div></div>").addClass("toast-item-close").prependTo(d).html(g.closeText).click(function () {
+                c().toastmessage("removeToast", d, g)
+            });
+            h = c("<div></div>").addClass("toast-item-image").addClass("toast-item-image-" + g.type).prependTo(d);
+            if (navigator.userAgent.match(/MSIE 6/i)) {
+                j.css({top: document.documentElement.scrollTop});
+            }
+            if (!g.sticky) {
+                setTimeout(function () {
+                    c().toastmessage("removeToast", d, g)
+                }, g.stayTime)
+            }
+            return d;
+        }, showErrorToast: function (e) {
+            var d = {text: e, type: "error"};
+            return c().toastmessage("showToast", d);
+        }, removeToast: function (e, d) {
+            e.animate({opacity: "0"}, 600, function () {
+                e.parent().animate({height: "0px"}, 300, function () {
+                    e.parent().remove();
+                })
+            });
+            if (d && d.close !== null) {
+                d.close();
+            }
+        }
+    };
+    c.fn.toastmessage = function (d) {
+        if (a[d]) {
+            return a[d].apply(this, Array.prototype.slice.call(arguments, 1))
+        } else {
+            if (typeof d === "object" || !d) {
+                return a.init.apply(this, arguments);
+            } else {
+                c.error("Method " + d + " does not exist on jQuery.toastmessage");
+            }
+        }
+    }
+})(jQuery);
+function alert(message) {
+    $().toastmessage('showErrorToast', message);
+}
