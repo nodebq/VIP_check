b15c69044b9185c92d06485f89a5f45cf0a431c8
Qrex <471597503@qq.com>
2016-04-17 15:10:06 +0800

    添加注释和修改readme

--- routes/index.js
+++ routes/index.js
@@ -16,34 +16,34 @@ var new2016 = require('../controller/new.js');
 //    next();
 //});
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res) {//把主页指向会员验证
     res.render('index', { title: 'Express' });
 });
 
 
-router.get('/check', function (req, res) {
+router.get('/check', function (req, res) {//会员验证接口
     res.header("Access-Control-Allow-Origin", "*");
     check.do(req,res);
 });
 
-router.get('/new', function (req, res) {
+router.get('/new', function (req, res) {//2016年招新表单提交接口
     res.header("Access-Control-Allow-Origin", "*");
     new2016.do(req,res);
 });
 
-router.get('/api/user', function (req, res) {
+router.get('/api/user', function (req, res) {//2016年招新后台用户列表
     res.header("Access-Control-Allow-Origin","*");
     new2016.select(req,res);
 });
-router.get('/api/userInfo', function (req, res) {
+router.get('/api/userInfo', function (req, res) {//2016年招新后台个人详细信息
     res.header("Access-Control-Allow-Origin","*");
     new2016.getUserInfo(req,res);
 });
-router.get('/api/evaluation', function (req, res) {
+router.get('/api/evaluation', function (req, res) {//2016年招新后台提交接口
     res.header("Access-Control-Allow-Origin","*");
     new2016.updateEvaluation(req,res);
 });
-router.get('/checkIn', function (req, res) {
+router.get('/checkIn', function (req, res) {//2016年签到接口
     res.header("Access-Control-Allow-Origin","*");
     new2016.checkIn(req,res);
 });
